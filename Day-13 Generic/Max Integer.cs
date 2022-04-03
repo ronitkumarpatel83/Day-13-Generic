867fea1ba4e150babcd170987bda68911e98e0fb
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-03 15:19:22 +0530

    [Ronit] Added : Max Integer

---
+++ Day-13 Generic/Max Integer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Generic
+{
+    internal class MaxInteger
+    {
+        public static int Max(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("First integer is maximum" + first);
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("Second Integer is Maximum " + second);
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) >= 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) > 0 && third.CompareTo(first) >= 0)
+            {
+                Console.WriteLine("Third Integer is maximum" + third);
+                return third;
+            }
+            if (first.CompareTo(second) == 0 && first.CompareTo(third) == 0 && second.CompareTo(third) == 0)
+            {
+                Console.WriteLine("All three integers are same");
+            }
+            return first;
+
+
+        }
+    }
+}
--- Day-13 Generic/Program.cs
+++ Day-13 Generic/Program.cs
@@ -11,7 +11,14 @@ namespace Day_13_Generic
         static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Problem Using Generic");
-            Console.ReadLine();
+            Console.WriteLine("<<<<<<<<<<<<<Generic>>>>>>>>>>>>>>>");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to Greater between 3 integer program ");
+            Console.WriteLine("Enter 3 Integer Value");
+            int fst = int.Parse(Console.ReadLine());
+            int snd = int.Parse(Console.ReadLine());
+            int trd = int.Parse(Console.ReadLine());
+            MaxInteger.Max(fst, snd, trd);
         }
     }
 }
