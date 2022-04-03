4a505a1e5376b15eebbb6ea24d698c4617bfaebe
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-03 20:40:06 +0530

    [Ronit] Added : Max Float

---
+++ Day-13 Generic/Max Float.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Generic
+{
+    internal class MaxFloat
+    {
+        public static double Float(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
+                    first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("First Float value is maximum" + first);
+                return first;
+            }
+
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
+                    second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("Second Float value is Maximum " + second);
+                return second;
+            }
+            if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) >= 0 && third.CompareTo(first) > 0 ||
+                    third.CompareTo(second) > 0 && third.CompareTo(first) >= 0)
+            {
+                Console.WriteLine("Third Float value is maximum" + third);
+                return third;
+            }
+            if (first.CompareTo(second) == 0 && first.CompareTo(third) == 0 && second.CompareTo(third) == 0)
+            {
+                Console.WriteLine("All three Floats Value are same");
+            }
+            return first;
+
+
+        }
+    }
+}
--- Day-13 Generic/Program.cs
+++ Day-13 Generic/Program.cs
@@ -13,12 +13,31 @@ namespace Day_13_Generic
             Console.WriteLine("Find Maximum Problem Using Generic");
             Console.WriteLine("<<<<<<<<<<<<<Generic>>>>>>>>>>>>>>>");
             Console.WriteLine();
-            Console.WriteLine("Welcome to Greater between 3 integer program ");
-            Console.WriteLine("Enter 3 Integer Value");
-            int fst = int.Parse(Console.ReadLine());
-            int snd = int.Parse(Console.ReadLine());
-            int trd = int.Parse(Console.ReadLine());
-            MaxInteger.Max(fst, snd, trd);
+            Console.WriteLine("1.Find maximum of three Integer number\n2.Find maximum of three Float number");
+            Console.WriteLine("3.Find maximum of string");
+            Console.WriteLine("Enter your choice : ");
+            int ch = Convert.ToInt32(Console.ReadLine());   // Storing user's choice
+            switch (ch)
+            {
+                case 1:
+                    Console.WriteLine("Welcome to Greater between 3 integer program ");
+                    Console.WriteLine("Enter 3 Integer Value");
+                    int fst = int.Parse(Console.ReadLine());
+                    int snd = int.Parse(Console.ReadLine());
+                    int trd = int.Parse(Console.ReadLine());
+                    MaxInteger.Max(fst, snd, trd);
+                    break;
+                case 2:
+                    Console.WriteLine("Welcome to Greater between 3 Floats program ");
+                    Console.WriteLine("Enter 3 Floats Value");
+                    double f = double.Parse(Console.ReadLine());
+                    double s = double.Parse(Console.ReadLine());
+                    double t = double.Parse(Console.ReadLine());
+                    MaxFloat.Float(f, s, t);
+                    break;
+                
+            }
+            
         }
     }
 }
