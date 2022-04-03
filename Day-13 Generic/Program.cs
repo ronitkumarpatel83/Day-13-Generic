632d43bfbd0436309f850b578c30310ae778abd2
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-03 15:12:16 +0530

    [Ronit] Added : Maximum Problem Using Generic

---
+++ Day-13 Generic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Problem Using Generic");
+            Console.ReadLine();
+        }
+    }
+}
